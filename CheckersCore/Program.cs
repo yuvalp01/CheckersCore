9fc5f66492a5141b4252852d2087ec1468e23359
Yuval <yuvalp01@gmail.com>
2019-07-16 14:36:01 +0200

    Add project files.

---
+++ CheckersCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckersCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
